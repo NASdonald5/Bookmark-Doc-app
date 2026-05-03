5675b4711dccf3decfc3f1ee70770b045ab5e8df
TesDon <testingdonald5@gmail.com>
2026-05-03 11:32:35 +0000

    updated

--- templates/install-Bookmarkapp.sh
+++ templates/install-Bookmarkapp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+#sudo nano install-Bookmarkapp.sh
+# Then copy the contents and run the script.
+# sudo ./install-Bookmarkapp.sh
 sudo chmod +x install-Bookmarkapp.sh
 #remove previous versions if any.
 sudo rm -rf  Bookmark-Doc-app/
